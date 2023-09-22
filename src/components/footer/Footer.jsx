9e57625d0973fbc47c38ae96874735aa5eb76a2e
Yamil <yamilbl@code.edu.az>
2023-09-22 17:40:34 +0400

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react';
-import './footer.scss'
+import { useState } from "react";
+import "./footer.scss";
+import { GrLocation } from "react-icons/gr";
+import { IconContext } from "react-icons/lib";
 
 const Footer = () => {
-
-
   return (
-    <div className='footer'>
-      
+    <div className="footer">
+      <div className="upFooter">
+        <div className="boxFooter">
+          <span className="icon"><GrLocation /></span>
+          <span>Address</span>
+          <p>
+            97845 Baker st. 567 <br /> Los Angeles - US
+          </p>
+        </div>
+        <div className="boxFooter">
+          <span>
+            <GrLocation />
+          </span>
+          <span>Address</span>
+          <p>97845 Baker st. 567 Los Angeles - US</p>
+        </div>
+        <div className="boxFooter">
+          <span>
+            <GrLocation />
+          </span>
+          <span>Address</span>
+          <p>97845 Baker st. 567 Los Angeles - US</p>
+        </div>
+        <div className="boxFooter">
+          <h4>Keep in touch</h4>
+          <div className="emailSend">
+            <input type="text" placeholder="email"/>
+            <div className="sendBtn"></div>
+          </div>
+        </div>
+      </div>
+      <div className="downFooter"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
