e8dddbcb1fedebdf15385f6f764d6e187daae84d
Yamil <yamilbl@code.edu.az>
2023-09-23 01:02:00 +0400

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,42 +1,80 @@
-import { useState } from "react";
 import "./footer.scss";
-import { GrLocation } from "react-icons/gr";
-import { IconContext } from "react-icons/lib";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import WifiCalling3OutlinedIcon from "@mui/icons-material/WifiCalling3Outlined";
+import fbc from "../../assets/socials/facebook_icon.svg";
+import ins from "../../assets/socials/instagram_icon.svg";
+import twt from "../../assets/socials/twitter_icon.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="upFooter">
         <div className="boxFooter">
-          <span className="icon"><GrLocation /></span>
-          <span>Address</span>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "center",
+            }}
+          >
+            <span className="icon">
+              <LocationOnOutlinedIcon fontSize="medium" />
+            </span>
+            <span className="title">Address</span>
+          </div>
           <p>
             97845 Baker st. 567 <br /> Los Angeles - US
           </p>
         </div>
         <div className="boxFooter">
-          <span>
-            <GrLocation />
-          </span>
-          <span>Address</span>
-          <p>97845 Baker st. 567 Los Angeles - US</p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "center",
+            }}
+          >
+            <span className="icon">
+              <WifiCalling3OutlinedIcon fontSize="medium" />
+            </span>
+            <span className="title">Reservations</span>
+          </div>
+          <p>
+            +94 423-23-221 <br />
+            reservations@Foores.com
+          </p>
         </div>
         <div className="boxFooter">
-          <span>
-            <GrLocation />
+          <span className="icon">
+            <AccessTimeOutlinedIcon fontSize="medium" />
           </span>
-          <span>Address</span>
-          <p>97845 Baker st. 567 Los Angeles - US</p>
+          <span className="title">İş saatları</span>
+          <p>
+            Mon - Sat: 10am - 11pm
+            <br /> Sunday: Closed
+          </p>
         </div>
+
         <div className="boxFooter">
-          <h4>Keep in touch</h4>
+          <h5>Əlaqə saxlayın</h5>
           <div className="emailSend">
-            <input type="text" placeholder="email"/>
+            <input type="text" placeholder="E-poçt" />
             <div className="sendBtn"></div>
           </div>
         </div>
       </div>
-      <div className="downFooter"></div>
+      <hr />
+      <div className="downFooter">
+        <div className="left">
+          © Adalat Restaurant - Bütün hüquqlar qorunur.
+        </div>
+        <div className="socials">
+          <img src={fbc} alt="" />
+          <img src={ins} alt="" />
+          <img src={twt} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
