cf82efcb6a946212da6b7efad2b109a90b513a94
Yamil <yamilbl@code.edu.az>
2023-09-23 18:24:53 +0400

    www

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,25 @@
-import './about.scss'
+import "./about.scss";
+import img from "./menuImg.png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="mainAbout">
+      <div className="menuPage">
+        <div className="menu">
+          <div className="img">
+            <img src={img} />
+          </div>
+          <h1>Haqqımızda</h1>
+          <p>2005-ci ildən ləzzətli və dadlı yeməklər bişiririk</p>
+        </div>
+      </div>
+      <div className="galleryTitle">
+         <h1 className="mainMenuTitle">Here some pictures</h1>
+         <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
+
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,25 @@
-import './gallery.scss'
+import "./gallery.scss";
+import img from "./menuImg.png";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <div className="mainGallery">
+      <div className="menuPage">
+        <div className="menu">
+          <div className="img">
+            <img src={img} />
+          </div>
+          <h1>Qalereya</h1>
+          <p>2005-ci ildən ləzzətli və dadlı yeməklər bişiririk</p>
+        </div>
+      </div>
+      <div className="galleryTitle">
+         <h1 className="mainMenuTitle">Here some pictures</h1>
+         <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
+
