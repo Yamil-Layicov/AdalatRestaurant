f929201d5d31f382b687eb02afa9ba2d1ff1f390
Yamil <yamilbl@code.edu.az>
2023-09-24 11:56:02 +0400

    configured  modal

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -4,25 +4,71 @@ import img from "./menuImg.png";
 import Modal from "./modal/Modal";
 
 const Gallery = () => {
-
   const images = [
-    {id:1, title:"https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"},
-    {id:2, title:"https://images.unsplash.com/photo-1610538625499-635bf13427b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"},
-    {id:3, title:"https://images.unsplash.com/photo-1634842611431-5d3c9bb9cbc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdG91cmFudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"},
-    {id:4, title:"https://images.unsplash.com/photo-1651873491993-29f701fa520d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJlc3RvdXJhbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"},
-    {id:5, title:"https://images.unsplash.com/photo-1657321185221-10dbdc729fe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"},
-    {id:6, title:"https://images.unsplash.com/photo-1631049421483-bbcab9b66473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"},
-    {id:7, title:"https://images.unsplash.com/photo-1679432116921-89f5b227bffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"},
-    {id:8, title:"https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"},
-    {id:9, title:"https://images.unsplash.com/photo-1679432116921-89f5b227bffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"},
-    {id:10, title:"https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"},
-    {id:11, title:"https://images.unsplash.com/photo-1657321185221-10dbdc729fe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"},
-    {id:12, title:"https://images.unsplash.com/photo-1631049421483-bbcab9b66473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"},
-  ]
+    {
+      id: 1,
+      title:
+        "https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id: 2,
+      title:
+        "https://images.unsplash.com/photo-1610538625499-635bf13427b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80",
+    },
+    {
+      id: 3,
+      title:
+        "https://images.unsplash.com/photo-1679432116921-89f5b227bffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      id: 4,
+      title:
+        "https://images.unsplash.com/photo-1651873491993-29f701fa520d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJlc3RvdXJhbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 5,
+      title:
+        "https://images.unsplash.com/photo-1657321185221-10dbdc729fe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      id: 6,
+      title:
+        "https://images.unsplash.com/photo-1631049421483-bbcab9b66473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id: 7,
+      title:
+        "https://images.unsplash.com/photo-1679432116921-89f5b227bffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      id: 8,
+      title:
+        "https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id: 9,
+      title:
+        "https://images.unsplash.com/photo-1679432116921-89f5b227bffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      id: 10,
+      title:
+        "https://images.unsplash.com/photo-1601893211509-81b6d03e46a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id: 11,
+      title:
+        "https://images.unsplash.com/photo-1657321185221-10dbdc729fe9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      id: 12,
+      title:
+        "https://images.unsplash.com/photo-1631049421483-bbcab9b66473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+  ];
 
   const [showModal, setShowModal] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-
 
   const openModal = (index) => {
     setCurrentIndex(index);
@@ -38,7 +84,9 @@ const Gallery = () => {
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   return (
@@ -53,16 +101,17 @@ const Gallery = () => {
         </div>
       </div>
       <div className="galleryTitle">
-         <h1 className="mainMenuTitle">Here some pictures</h1>
-         <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+        <h1 className="mainMenuTitle">Here some pictures</h1>
+        <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
       </div>
-
-      <div className="imgBox">
-        {images.map((img, index) => (
-          <div key={img.id} onClick={() => openModal(index)}>
-            <img src={img.title} alt={`Image ${img.id}`} />
-          </div>
-        ))}
+      <div className="imgBoxContent">
+        <div className="imgBox">
+          {images.map((img, index) => (
+            <div key={img.id} onClick={() => openModal(index)}>
+              <img src={img.title} alt={`Image ${img.id}`} />
+            </div>
+          ))}
+        </div>
       </div>
 
       <Modal
@@ -78,4 +127,3 @@ const Gallery = () => {
 };
 
 export default Gallery;
-
